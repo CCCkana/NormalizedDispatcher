a312fd2836105f176eb8476cd2554f95a8a7a35f
Kana Mo(LTSC) <toolManwang@qq.com>
2021-03-24 08:13:40 -0700

    Something added.

--- NormalizedDispatcher/Program.cs
+++ NormalizedDispatcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NormalizedDispatcher.Shared;
 
 namespace NormalizedDispatcher
 {
@@ -6,7 +7,7 @@ namespace NormalizedDispatcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ NormalizedDispatcher/Shared.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NormalizedDispatcher.Shared
+{
+    public record StaticParameters
+    {
+        public double zMax1;
+        public double zMax2;
+        public double zMin;
+        public double K;
+
+        public StaticParameters()
+        {
+            zMax1 = 780.0;
+            zMax2 = 773.1;
+            zMin = 731.0;
+            K = 8.6;
+        }
+
+
+    }
+
+    public class Loader
+    {
+        private string basePath;
+        private string folderPath;
+        public Loader()
+        {
+            basePath = Directory.GetCurrentDirectory();
+            folderPath = Path.Combine(basePath, "Data");
+        }
+    }
+}
